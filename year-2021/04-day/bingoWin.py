b21d1c8013887614698330fc323a3d9d54845b66
SlimRunner <dv.polygon@gmail.com>
2021-12-03 23:11:05 -0800

    ✔️ day 4 - part 1

--- year-2021/04-day/bingoWin.py
+++ year-2021/04-day/bingoWin.py
@@ -17,3 +17,34 @@ def getLines(fn):
     height = len(cards[0]) // width
     return (nums, cards, width, height)
 
+def genWinPattern(w, h):
+    output = []
+    wbf = (1 << w) - 1
+    hbf = sum([1 << i for i in range(0, w * h, w)])
+    for x in range(0, w):
+        output.append(wbf << x * w)
+    for y in range(0, h):
+        output.append(hbf << y)
+    return output
+
+def bitMatch(value, bitfield):
+    for target in bitfield:
+        if value & target == target: return True
+
+def bingoFirstWin(fn):
+    calls, cards, w, h = getLines(fn)
+    marks = [0] * len(cards)
+    winPatterns = genWinPattern(w, h)
+    for nthCall, call in enumerate(calls):
+        for idx, card in enumerate(cards):
+            try:
+                match = cards[idx].index(call)
+                marks[idx] |= 1 << match
+                if bitMatch(marks[idx], winPatterns):
+                    return (cards[idx], calls[0:nthCall + 1])
+            except Exception as e:
+                pass
+
+winner, calls = bingoFirstWin("bingoCards.in.txt")
+diff = list(set(winner) - set(calls))
+print(sum(diff) * calls.pop())
