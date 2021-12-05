690c8fa119295e9c72c29b7594a08a32e34d2fb6
SlimRunner <dv.polygon@gmail.com>
2021-12-04 23:55:38 -0800

    🎨 day 5 - tidy up output

--- year-2021/05-day/thermal.py
+++ year-2021/05-day/thermal.py
@@ -22,32 +22,37 @@ def getLines(fn):
     return (output, nMax, nMin)
 
 
-coords, nMax, nMin = getLines("thermalLines.in.txt")
-visitMap = {}
+def solution(**kwargs):
+    doDiags = kwargs.get("doDiags", False)
+    coords, nMax, nMin = getLines("thermalLines.in.txt")
+    visitMap = {}
 
-for coord in coords:
-    x1, y1, x2, y2 = coord
-    if x1 == x2:
-        for y in range(min(y1, y2), max(y1, y2) + 1):
-            if (x1, y) in visitMap:
-                visitMap[(x1, y)] += 1
-            else:
-                visitMap[(x1, y)] = 1
-    elif y1 == y2:
-        for x in range(min(x1, x2), max(x1, x2) + 1):
-            if (x, y1) in visitMap:
-                visitMap[(x, y1)] += 1
-            else:
-                visitMap[(x, y1)] = 1
-    elif abs(x2 - x1) == abs(y2 - y1):
-        xs = 1 if x2 > x1 else -1
-        ys = 1 if y2 > y1 else -1
-        xR1, xR2 = x1, x2 + xs
-        yR1, yR2 = y1, y2 + ys
-        for x, y in zip(range(xR1, xR2, xs), range(yR1, yR2, ys)):
-            if (x, y) in visitMap:
-                visitMap[(x, y)] += 1
-            else:
-                visitMap[(x, y)] = 1
+    for coord in coords:
+        x1, y1, x2, y2 = coord
+        if x1 == x2:
+            for y in range(min(y1, y2), max(y1, y2) + 1):
+                if (x1, y) in visitMap:
+                    visitMap[(x1, y)] += 1
+                else:
+                    visitMap[(x1, y)] = 1
+        elif y1 == y2:
+            for x in range(min(x1, x2), max(x1, x2) + 1):
+                if (x, y1) in visitMap:
+                    visitMap[(x, y1)] += 1
+                else:
+                    visitMap[(x, y1)] = 1
+        elif doDiags and abs(x2 - x1) == abs(y2 - y1):
+            xs = 1 if x2 > x1 else -1
+            ys = 1 if y2 > y1 else -1
+            xR1, xR2 = x1, x2 + xs
+            yR1, yR2 = y1, y2 + ys
+            for x, y in zip(range(xR1, xR2, xs), range(yR1, yR2, ys)):
+                if (x, y) in visitMap:
+                    visitMap[(x, y)] += 1
+                else:
+                    visitMap[(x, y)] = 1
+    count = len([v for k,v in visitMap.items() if v > 1])
+    return count
 
-print(len([v for k,v in visitMap.items() if v > 1]))
+print("p1: ", solution())
+print("p2: ", solution(doDiags = True))
