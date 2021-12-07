550c71c596531d2ba3550d71c532f106e772a4c7
SlimRunner <dv.polygon@gmail.com>
2021-12-06 18:38:04 -0800

    ✔️ day 6 - part 2

--- year-2021/06-day/solve.py
+++ year-2021/06-day/solve.py
@@ -1,5 +1,12 @@
 import re
 
+# Demonstrates visually that numbers can be grouped by modulo
+# https://www.desmos.com/calculator/yzrkxpesfa
+# Visualizes the modulo 3 group
+# https://www.desmos.com/calculator/p3q44zsxn6
+# Visualizes all modulo n groups up to 7
+# https://www.desmos.com/calculator/8x5fseraet
+
 def getLines(fn):
     output = []
     with open(fn) as f:
@@ -7,16 +14,41 @@ def getLines(fn):
         output = [int(x) for x in re.findall("\d+", text)]
     return output
 
-def solution(states, days):
-    for _ in range(days):
-        idx = 0
-        iterLen = len(states)
-        while idx < iterLen:
-            states[idx] -= 1
-            if states[idx] < 0:
-                states[idx] = 6
-                states.append(8)
-            idx += 1
-    return len(states)
+def countSpans(khp, cycles, kernel, spawnHP):
+    kernelHP = khp[:]
+    # kernel should be treated as the modulo not the upper limit
+    mod = lambda n, k: (n + k) % kernel
+    kth = kernel
+    # hp of members by modulo groups (kernel group count/iter)
+    kgCount = [0] * kth
+    kgIter = [0] * kth
+    # hp of members by spawn groups (spawn group count/iter)
+    sth = range(1, spawnHP - kernel + 2)
+    sgCount = [0] * kth # (kth * spawnHP // kernel)
+    sgIter = [spawnHP] * kth # (kth * spawnHP // kernel)
+    for k in kernelHP:
+        kgCount[mod(k, 0)] += 1
+        kgIter[mod(k, 0)] = k
+    for iter in range(1, cycles + 1):
+        kgIter = [k - 1 if kgCount[i] > 0 else k for i, k in enumerate(kgIter)]
+        sgIter = [s - 1 if sgCount[i] > 0 else s for i, s in enumerate(sgIter)]
+        for i, sg, kg in zip(range(0, kth + 1), sgIter, kgIter):
+            oldkgCount = kgCount[i]
+            oldsgCount = sgCount[i]
+            if kg < 0:
+                kgIter[i] %= kernel
+                sgCount[mod(spawnHP, iter)] += oldkgCount
+                # sgIter[...] should already be 8
+            if sg < kernel:
+                kgCount[i] += oldsgCount
+                sgCount[i] = 0
+                kgIter[i] = sgIter[i]
+                sgIter[i] = spawnHP
+    return sum(kgCount) + sum(sgCount)
 
-print(solution(getLines("input.in.txt"), 80))
+def solution(states, days):
+    return countSpans(states, days, 7, 8)
+
+fishSeed = getLines("input.in.txt")
+print(solution(fishSeed, 80))
+print(solution(fishSeed, 256))
