03a0cf3db015dbd20835a78a7ce4477e0d8938cb
SlimRunner <dv.polygon@gmail.com>
2021-12-11 00:36:39 -0800

    ✔️ day 11 - part 1 & 2

--- year-2021/11-day/solve.py
+++ year-2021/11-day/solve.py
@@ -1,5 +1,3 @@
-import re
-
 def getLines(fn):
     output = []
     with open(fn) as f:
@@ -9,54 +7,65 @@ def getLines(fn):
             line = f.readline().strip()
     return output
 
-def convSum(grid):
-    carryStack = []
-    needCarry = False
-    NB = [(0,1),(0,-1),(1,0),(-1,0),(1,1),(-1,1),(1,-1),(-1,-1)]
+def capmax(grid, limit):
+    total = 0
     h = len(grid)
     w = len(grid[0])
     for y in range(0, h):
         for x in range(0, w):
-            val = grid[y][x]
-            if val == 0:
-                for neig in NB:
-                    xn, yn = neig[0] + x, neig[1] + y
-                    if 0 <= xn < w and 0 <= yn < h:
-                        grid[yn][xn] += 1
-                        if (grid[yn][xn] > 9):
-                            carryStack.append((xn, yn))
-                            grid[yn][xn] = 0
-    return carryStack
+            if grid[y][x] >= limit:
+                grid[y][x] = 0
+                total += 1
+    return total
 
-def carrySum(grid, locs):
-    carryStack = []
-    if len(locs) == 0: return False
-    NB = [(0,1),(0,-1),(1,0),(-1,0),(1,1),(-1,1),(1,-1),(-1,-1)]
+def increase(grid, limit):
     h = len(grid)
     w = len(grid[0])
-    for loc in locs:
-        x, y = loc
-        for neig in NB:
-            xn, yn = neig[0] + x, neig[1] + y
-            if 0 <= xn < w and 0 <= yn < h:
-                grid[yn][xn] += 1
-                if (grid[yn][xn] > 9):
-                    carryStack.append((xn, yn))
-                    grid[yn][xn] = 0
-    return carryStack
+    stack = []
+    for _ in range(0, 1):
+        for y in range(0, h):
+            for x in range(0, w):
+                grid[y][x] += 1
+                if grid[y][x] == limit:
+                    stack.append((x, y))
+    while len(stack) > 0:
+        loc = stack.pop()
+        stack.extend(bumpNeighbors(grid, loc, limit))
+
+def bumpNeighbors(grid, loc, limit):
+    NB = [(0,1),(0,-1),(1,0),(-1,0),(1,1),(-1,1),(1,-1),(-1,-1)]
+    h, w = len(grid), len(grid[0])
+    x, y = loc
+    stack = []
+    for neig in NB:
+        xn, yn = neig[0] + x, neig[1] + y
+        if 0 <= xn < w and 0 <= yn < h:
+            grid[yn][xn] += 1
+            if grid[yn][xn] == limit:
+                stack.append((xn, yn))
+    return stack
+
+def printGrid(grid):
+    print()
+    for rows in grid:
+        pout = ''.join([
+            str(n) if n < 10 else chr(55 + n)
+            for n in rows
+        ])
+        print(pout)
 
 grid = getLines("input.in.txt")
-print(grid)
-
-total = 0
-for _ in range(0, 100):
-    grid = [[(n + 1) % 10 for n in row] for row in grid]
-    cg = convSum(grid)
-    if len(cg) > 0:
-        cg = carrySum(grid, cg)
-    while len(cg) > 0:
-        cg = carrySum(grid, cg)
-    total += sum([n.count(0) for n in grid])
-
-print(grid)
-print(total)
+gLen = len(grid) * len(grid[0])
+zsum = 0
+zstep = 0
+t = 0
+while zstep < gLen:
+    if t == 100:
+        printGrid(grid)
+        print(zsum, "zeroes at t = 100")
+    increase(grid, 10)
+    zstep = capmax(grid, 10)
+    zsum += zstep
+    t += 1
+printGrid(grid)
+print("sync at t =", t)
