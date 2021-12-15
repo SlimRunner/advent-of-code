3ed8d6d2e8eaa3ca2b168f2656d45bf92a27f01f
SlimRunner <dv.polygon@gmail.com>
2021-12-15 15:12:07 -0800

    ✔️ day 12 - part 2

--- year-2021/12-day/solve.py
+++ year-2021/12-day/solve.py
@@ -31,14 +31,14 @@ def makeNodeArray(nodePairs):
         output[a][b] += 1
         output[b][a] += 1
     bigCaves = [i for k, i in uniqueNodes.items() if k.isupper()]
-    return (output, bigCaves)
+    return (output, bigCaves, uniqueNodes)
 
 def countPaths(nodes, exempt, visited, cave):
     # TIL: this is called backtracking
     if cave == len(nodes) - 1: return 1
     visited[cave] = True
     tour = [
-        i for i, p in enumerate(caveMap[cave])
+        i for i, p in enumerate(nodes[cave])
         if p != 0 and (not visited[i] or i in exempt)
     ]
     paths = 0
@@ -48,26 +48,34 @@ def countPaths(nodes, exempt, visited, cave):
     return paths
 
 def countPathsTwin(nodes, exempt, visited, cave, twin):
-    # TIL: this is called backtracking
-    if cave == len(nodes) - 1: return 1
-    visited[cave] = True
+    if cave == len(nodes) - 1:
+        return 1
+    visited[cave] += 1
     tour = [
-        i for i, p in enumerate(caveMap[cave])
-        if p != 0 and (not visited[i] or i in exempt)
+        i for i, p in enumerate(nodes[cave])
+        if p != 0 and (visited[i] == 0 or i in exempt)
     ]
+    trip2 = [
+        i for i, p in enumerate(nodes[cave])
+        if p != 0 and visited[i] != 0 and i not in exempt
+        and i not in [0, len(nodes) - 1]
+    ] if not twin else []
     paths = 0
     for t in tour:
         paths += countPathsTwin(nodes, exempt, visited, t, twin)
-    visited[cave] = False
+    for t2 in trip2:
+        paths += countPathsTwin(nodes, exempt, visited, t2, True)
+    visited[cave] -= 1
     return paths
 
 if __name__ == "__main__":
     nodePairs = getLines("input.in.txt")
-    nSize = len(nodePairs)
-    caveMap, bigCaves = makeNodeArray(nodePairs)
-    print(caveMap, '\n', bigCaves)
+    caveMap, bigCaves, caveNames = makeNodeArray(nodePairs)
+    nSize = len(caveMap)
+    # print(caveMap, '\n', bigCaves)
+    caveNames = {v:k for k,v in caveNames.items()}
     
     paths1 = countPaths(caveMap, bigCaves, [False] * nSize, 0)
-    paths2 = countPathsTwin(caveMap, bigCaves, [False] * nSize, 0, False)
-    print(paths1)
-    print(paths2)
+    paths2 = countPathsTwin(caveMap, bigCaves, [0] * nSize, 0, False)
+    print("part 1:", paths1)
+    print("part 2:", paths2)
