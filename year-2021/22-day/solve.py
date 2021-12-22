46cb0ca3314a8e6bec7d8a7470d200a3263574b4
SlimRunner <dv.polygon@gmail.com>
2021-12-22 02:58:36 -0800

    ✔️ day 22 - part 2

--- year-2021/22-day/solve.py
+++ year-2021/22-day/solve.py
@@ -1,35 +1,64 @@
 import sys
 import re
+from math import prod
+from functools import reduce
 
 def main(args):
+    progSize = 20
     pre = lambda x: x
     if "-ex" in args:
         data = getLines("data.ex.txt", pred = pre)
     else:
         data = getLines("data.in.txt", pred = pre)
-    # print(data)
-    # use a function that generates a polyhedra and at the end compute the volume of that
-    # for now use naive approach
-    c = [[[False for _ in range(-50, 51)] for _ in range(-50, 51)] for _ in range(-50, 51)]
-    print(len(c), len(c[0]), len(c[0][0]))
-    count = 0
-    for ins in data:
-        do, (x1, x2), (y1, y2), (z1, z2) = ins
-        if not (
-            -50 <= x1 <= 50 and -50 <= x2 <= 50 and
-            -50 <= y1 <= 50 and -50 <= y2 <= 50 and
-            -50 <= z1 <= 50 and -50 <= z2 <= 50): continue
-        x1, x2, y1, y2, z1, z2 = [min(max(i, -50), 50) + 50 for i in [x1, x2, y1, y2, z1, z2]]
-        for z in range(z1, z2 + 1):
-            for y in range(y1, y2 + 1):
-                for x in range(x1, x2 + 1):
-                    if c[z][y][x] != do:
-                        if do:
-                            count += 1
-                        else:
-                            count -= 1
-                    c[z][y][x] = do
-    print(count)
+    area = 0
+    boxes = []
+    for i, (act, x, y, z) in enumerate(data):
+        prog = int((i / len(data)) * progSize)
+        print("*" * prog + "." * (progSize - prog), end="\r")
+        if "-p1" in args and not (
+            -50 <= x[0] <= 50 and -50 <= x[1] <= 50 and
+            -50 <= y[0] <= 50 and -50 <= y[1] <= 50 and
+            -50 <= z[0] <= 50 and -50 <= z[1] <= 50): continue
+        # overlap found
+        ovf = False
+        ovBoxes = []
+        for op, *box in boxes:
+            if overArea([x, y, z], box) > 0:
+                ovf = True
+                ovBox = overBox([x, y, z], box)
+                if act == op:
+                    ovBoxes.append((not act, *ovBox))
+                else:
+                    ovBoxes.append((act, *ovBox))
+        if ovf:
+            boxes.extend(ovBoxes)
+        if act:
+            boxes.append((act, x, y, z))
+    for act, x, y, z in boxes:
+        area += getArea(x, y, z) * (1 if act else -1)
+    print(" " * progSize)
+    print("final area:", area)
+
+def overBox(b1, b2):
+    b3 = []
+    for comp in zip(b1, b2):
+        p1, p2 = zip(*comp)
+        b3.append((max(p1), min(p2)))
+    return b3
+
+def overArea(b1, b2):
+    res = 1
+    for comp in zip(b1, b2):
+        p1, p2 = zip(*comp)
+        res *= max(min(p2) - max(p1), 0)
+    return res
+
+# https://www.desmos.com/calculator/65xkacmplj
+def getArea(*coords):
+    p = 1
+    for coord in coords:
+        p *= abs(coord[0] - coord[1])
+    return p
 
 def getLines(fn, **kw):
     pred = (lambda x: x) if "pred" not in kw else kw["pred"]
@@ -39,7 +68,7 @@ def getLines(fn, **kw):
         while line:
             act = line.find("on") != -1
             region = re.findall("(?<=[x-z]=)(-?\d+)\.\.(-?\d+)", line)
-            region = tuple((int(a), int(b)) for (a, b) in region)
+            region = [(int(a), int(b) + 1) for (a, b) in region]
             line = line.strip()
             output.append((act, *region))
             line = f.readline()
