76e80ed20181b2a13ff34278112fd51fa1d535f7
SlimRunner <dv.polygon@gmail.com>
2022-11-30 22:34:09 -0800

    python template fix: read lines

--- templates/python/temp01.py
+++ templates/python/temp01.py
@@ -10,6 +10,7 @@ def main(args):
     if "-somearg" in args:
         pass # process or react to argument
     # print your output
+    print(f"lines: {lines}") # test message
     # print(f"part 1: {}")
     # print(f"part 2: {}")
 
@@ -17,12 +18,10 @@ def getLines(fn, **kw):
     pred = (lambda x: x) if "pred" not in kw else kw["pred"]
     output = []
     with open(fn) as f:
-        line = f.readline()[:-1]
-        l = len(line)
+        line = f.readline()
         while line:
-            line = line + " " * (l - len(line))
-            output.append([pred(c) for c in line])
-            line = f.readline()[:-1]
+            output.append(pred(line))
+            line = f.readline()
     return output
 
 if __name__ == '__main__':
