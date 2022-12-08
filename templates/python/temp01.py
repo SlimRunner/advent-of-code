51005ecd5a9418fc2d8c5e441bd8a6683de021a8
SlimRunner <dv.polygon@gmail.com>
2022-12-07 22:55:26 -0800

    python template format cleanup

--- templates/python/temp01.py
+++ templates/python/temp01.py
@@ -2,27 +2,27 @@ import sys
 import re
 
 def main(args):
-    pre = lambda x: x
-    if "-ex" in args:
-        lines = getLines("data.ex.txt", pred = pre)
-    else:
-        lines = getLines("data.in.txt", pred = pre)
-    if "-somearg" in args:
-        pass # process or react to argument
-    # print your output
-    print(f"lines: {lines}") # test message
-    # print(f"part 1: {}")
-    # print(f"part 2: {}")
+  pre = lambda x: x
+  if "-ex" in args:
+    lines = getLines("data.ex.txt", pred = pre)
+  else:
+    lines = getLines("data.in.txt", pred = pre)
+  if "-somearg" in args:
+    pass # process or react to argument
+  # print your output
+  print(f"lines: {lines}") # test message
+  # print(f"part 1: {}")
+  # print(f"part 2: {}")
 
 def getLines(fn, **kw):
-    pred = (lambda x: x) if "pred" not in kw else kw["pred"]
-    output = []
-    with open(fn) as f:
-        line = f.readline()
-        while line:
-            output.append(pred(line))
-            line = f.readline()
-    return output
+  pred = (lambda x: x) if "pred" not in kw else kw["pred"]
+  output = []
+  with open(fn) as f:
+    line = f.readline()
+    while line:
+      output.append(pred(line))
+      line = f.readline()
+  return output
 
 if __name__ == '__main__':
-    main(sys.argv)
+  main(sys.argv)
