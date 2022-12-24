0d1d2c180028912d60d9e22b266269a3d1209ffd
SlimRunner <dv.polygon@gmail.com>
2022-12-24 13:15:31 -0800

    python template update

--- templates/python/temp01.py
+++ templates/python/temp01.py
@@ -1,10 +1,13 @@
 import sys
+import glob
 import re
 
 def main(args):
   pre = lambda x: x.rstrip('\n')
   if "-ex" in args:
-    lines = getLines("data.ex.txt", pred = pre)
+    examples = glob.glob("*.ex.txt")
+    einx = (int(args["ex"][0]) if len(args["ex"]) else 0) % len(examples)
+    lines = getLines(examples[einx], pred = pre)
   else:
     lines = getLines("data.in.txt", pred = pre)
   if "-somearg" in args:
@@ -25,4 +28,12 @@ def getLines(fn, **kw):
   return output
 
 if __name__ == '__main__':
-  main(sys.argv)
+  args = {}
+  lkey = None
+  for arg in sys.argv:
+    if arg[0] == "-":
+      lkey = arg[1:]
+      args[lkey] = []
+    elif len(args) and lkey is not None:
+      args[lkey].append(arg)
+  main(args)
